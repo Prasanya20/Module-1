e952a2f873d375e6d681b6efe098d098c572332b
Prasanya20 <prasanya.medapati2006@gmail.com>
2026-07-22 09:44:25 +0530

    Update SingletonPatternExample.java

--- SingletonPatternExample.java
+++ SingletonPatternExample.java
@@ -1,9 +1,27 @@
-public class SingletonPatternExample {
-  public static void main(String[] args) {
-    Logger logger1 = Logger.getInstance();
-    Logger logger2 = Logger.getInstance();
-    logger1.log("Application Started");
-    logger2.log("Logging another message");
+class Logger {
+    
+    private static Logger instance = new Logger();
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application Started");
+        logger2.log("Logging another message");
 
         if (logger1 == logger2) {
             System.out.println("Only one Logger instance is created.");
